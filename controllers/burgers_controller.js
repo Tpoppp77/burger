ba80c1ffefd451c98c48afd4bd65f6cdfb861a55
Tpoppp <olson0002@yahoo.com>
2020-06-02 22:51:54 -0700

    completed controller

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -9,15 +9,35 @@ router.get("/", async (req,res) => {
 });
 
 router.post("/api/burgers", async (req,res) => {
+    const data = await burger.create(["burger_name", "devoured"], [req.body.burger_name, req.body.devoured]);
 
+    res.json({ id: data.insertId });
 });
 
 router.put("/api/burgers/:id", async (req,res) => {
+    let condition = `id = ${req.params.id}`;
 
+    console.log("condition", condition);
+  
+    const data = await burger.update({ devoured: req.body.devoured }, condition);
+  
+    if (data.changedRows === 0) {
+      res.status(404).end();
+    }
+  
+    res.status(200).end();
 })
 
 router.delete("/api/burgers/:id", (req,res) => {
+    let condition = `id = ${req.params.id}`;
 
+    const data = await burger.delete(condition);
+  
+    if (data.affectedRows === 0) {
+      res.status(404).end();
+    }
+  
+    res.status(200).end();
 })
 
 module.exports = router;
